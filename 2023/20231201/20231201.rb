79e0ba10e08acf2e76c8c5f81fcd771bee8fbaf4
Michael Coleman <colemanmc02@gmail.com>
2023-12-01 10:01:24 -0500

    20231201 part 1

--- 2023/20231201/20231201.rb
+++ 2023/20231201/20231201.rb
@@ -1,8 +1,33 @@
 require './utils/get_data'
 
-def runner
-    data = GetData.get_daily_data(2023,1)
-    data
+def get_numbers(string)
+  numbers = %w[1 2 3 4 5 6 7 8 9]
+
+  results = ''
+  split_string = string.split('')
+
+  split_string.each do |char|
+    results += char if numbers.include? char
+  end
+
+  results
 end
 
-print runner()
+def format_strings(data)
+  data.split("\n").map do |line|
+    numbers = get_numbers(line)
+    numbers[0] + numbers[numbers.length - 1]
+  end
+end
+
+def sum_numbers(values)
+  values.inject(0) { |sum, x| sum + x.to_i }
+end
+
+def runner
+  data = GetData.get_daily_data(2023, 1)
+  values = format_strings(data)
+  sum_numbers(values)
+end
+
+print runner
--- utils/get_data.rb
+++ utils/get_data.rb
@@ -4,9 +4,9 @@ require 'dotenv'
 Dotenv.load
 
 class GetData
-    def self.get_daily_data(year_number, day_number)
+  def self.get_daily_data(year_number, day_number)
         open(
-            "https://adventofcode.com/#{year_number}/day/#{day_number}/input",
+                "https://adventofcode.com/#{year_number}/day/#{day_number}/input",
             "Cookie" => ENV["COOKIE"]
         ).read
     end
